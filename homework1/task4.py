2f494a740fda6e0b78d16152053ab26e257528a1
JamieWadge <jamiecwadge@gmail.com>
2026-09-25 03:16:21 +0000

    Finsihed task4

--- homework1/task4.py
+++ homework1/task4.py
@@ -1,1 +1,14 @@
-def calculate
+def calculate_discount(price, discount):
+    return price - (price * discount / 100)
+
+def test_integer_price_integer_discount():
+    assert calculate_discount(100, 10) == 90
+
+def test_integer_price_float_discount():
+    assert calculate_discount(100, 33.3) == 66.7
+
+def test_float_price_integer_dicsount():
+    assert calculate_discount(50.5, 10) == 45.45
+
+def test_float_price_float_discount():
+    assert calculate_discount(50.5, 33.3) == 33.6835
